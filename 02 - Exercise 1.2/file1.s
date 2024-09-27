96b88015b673d3c5b918ee4065bb0d024b8e440e
asm <asm@asm.myguest.virtualbox.org>
2024-09-26 20:51:26 -0600

    Now with makefiles!

--- 02 - Exercise 1.2/file1.s
+++ 02 - Exercise 1.2/file1.s
@@ -4,6 +4,8 @@
 	.attribute unaligned_access, 0
 	.attribute stack_align, 16
 	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
 	.section	.rodata
 	.align	2
 .LC0:
@@ -13,10 +15,19 @@
 	.globl	main
 	.type	main, @function
 main:
+.LFB0:
+	.file 1 "file1.c"
+	.loc 1 3 16
+	.cfi_startproc
 	addi	sp,sp,-32
+	.cfi_def_cfa_offset 32
 	sw	ra,28(sp)
 	sw	s0,24(sp)
+	.cfi_offset 1, -4
+	.cfi_offset 8, -8
 	addi	s0,sp,32
+	.cfi_def_cfa 8, 0
+	.loc 1 4 14
 	lui	a5,%hi(.LC0)
 	addi	a5,a5,%lo(.LC0)
 	lw	a2,0(a5)
@@ -27,16 +38,227 @@ main:
 	sw	a4,-24(s0)
 	lhu	a5,12(a5)
 	sh	a5,-20(s0)
+	.loc 1 5 3
 	addi	a5,s0,-32
 	li	a2,13
 	mv	a1,a5
 	li	a0,1
 	call	write
+	.loc 1 6 10
 	li	a5,0
+	.loc 1 7 1
 	mv	a0,a5
 	lw	ra,28(sp)
+	.cfi_restore 1
 	lw	s0,24(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 32
 	addi	sp,sp,32
+	.cfi_def_cfa_offset 0
 	jr	ra
+	.cfi_endproc
+.LFE0:
 	.size	main, .-main
+.Letext0:
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.4byte	0x7b
+	.2byte	0x4
+	.4byte	.Ldebug_abbrev0
+	.byte	0x4
+	.byte	0x1
+	.4byte	.LASF2
+	.byte	0xc
+	.4byte	.LASF3
+	.4byte	.LASF4
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	.Ldebug_line0
+	.byte	0x2
+	.4byte	.LASF5
+	.byte	0x1
+	.byte	0x3
+	.byte	0x5
+	.4byte	0x4f
+	.4byte	.LFB0
+	.4byte	.LFE0-.LFB0
+	.byte	0x1
+	.byte	0x9c
+	.4byte	0x4f
+	.byte	0x3
+	.string	"str"
+	.byte	0x1
+	.byte	0x4
+	.byte	0xe
+	.4byte	0x66
+	.byte	0x2
+	.byte	0x91
+	.byte	0x60
+	.byte	0
+	.byte	0x4
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.byte	0x5
+	.4byte	0x79
+	.4byte	0x66
+	.byte	0x6
+	.4byte	0x6b
+	.byte	0xd
+	.byte	0
+	.byte	0x7
+	.4byte	0x56
+	.byte	0x8
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF0
+	.byte	0x8
+	.byte	0x1
+	.byte	0x8
+	.4byte	.LASF1
+	.byte	0x7
+	.4byte	0x72
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.byte	0x1
+	.byte	0x11
+	.byte	0x1
+	.byte	0x25
+	.byte	0xe
+	.byte	0x13
+	.byte	0xb
+	.byte	0x3
+	.byte	0xe
+	.byte	0x1b
+	.byte	0xe
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x10
+	.byte	0x17
+	.byte	0
+	.byte	0
+	.byte	0x2
+	.byte	0x2e
+	.byte	0x1
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x27
+	.byte	0x19
+	.byte	0x49
+	.byte	0x13
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x96,0x42
+	.byte	0x19
+	.byte	0x1
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x3
+	.byte	0x34
+	.byte	0
+	.byte	0x3
+	.byte	0x8
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x4
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0x8
+	.byte	0
+	.byte	0
+	.byte	0x5
+	.byte	0x1
+	.byte	0x1
+	.byte	0x49
+	.byte	0x13
+	.byte	0x1
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x6
+	.byte	0x21
+	.byte	0
+	.byte	0x49
+	.byte	0x13
+	.byte	0x2f
+	.byte	0xb
+	.byte	0
+	.byte	0
+	.byte	0x7
+	.byte	0x26
+	.byte	0
+	.byte	0x49
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x8
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0xe
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.4byte	0x1c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	0
+	.4byte	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF4:
+	.string	"/home/asm/Documents/An Introduction to Assm Prog with RISC-V/02 - Exercise 1.2"
+.LASF3:
+	.string	"file1.c"
+.LASF0:
+	.string	"unsigned int"
+.LASF2:
+	.string	"GNU C17 10.2.0 -march=rv32i -mabi=ilp32 -g"
+.LASF5:
+	.string	"main"
+.LASF1:
+	.string	"char"
 	.ident	"GCC: () 10.2.0"
--- 02 - Exercise 1.2/file2.s
+++ 02 - Exercise 1.2/file2.s
@@ -4,14 +4,24 @@
 	.attribute unaligned_access, 0
 	.attribute stack_align, 16
 	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
 	.align	2
 	.globl	exit
 	.type	exit, @function
 exit:
+.LFB0:
+	.file 1 "file2.c"
+	.loc 1 3 21
+	.cfi_startproc
 	addi	sp,sp,-32
+	.cfi_def_cfa_offset 32
 	sw	s0,28(sp)
+	.cfi_offset 8, -4
 	addi	s0,sp,32
+	.cfi_def_cfa 8, 0
 	sw	a0,-20(s0)
+	.loc 1 4 3
 	lw	a5,-20(s0)
  #APP
 # 4 "file2.c" 1
@@ -19,22 +29,35 @@ exit:
 li a7, 93           # syscall exit (93) 
 ecall
 # 0 "" 2
+	.loc 1 12 1
  #NO_APP
 	nop
 	lw	s0,28(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 32
 	addi	sp,sp,32
+	.cfi_def_cfa_offset 0
 	jr	ra
+	.cfi_endproc
+.LFE0:
 	.size	exit, .-exit
 	.align	2
 	.globl	write
 	.type	write, @function
 write:
+.LFB1:
+	.loc 1 14 49
+	.cfi_startproc
 	addi	sp,sp,-32
+	.cfi_def_cfa_offset 32
 	sw	s0,28(sp)
+	.cfi_offset 8, -4
 	addi	s0,sp,32
+	.cfi_def_cfa 8, 0
 	sw	a0,-20(s0)
 	sw	a1,-24(s0)
 	sw	a2,-28(s0)
+	.loc 1 15 3
 	lw	a5,-20(s0)
 	lw	a4,-24(s0)
 	lw	a3,-28(s0)
@@ -46,23 +69,353 @@ mv a2, a3           # size
 li a7, 64           # syscall write (64) 
 ecall
 # 0 "" 2
+	.loc 1 25 1
  #NO_APP
 	nop
 	lw	s0,28(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 32
 	addi	sp,sp,32
+	.cfi_def_cfa_offset 0
 	jr	ra
+	.cfi_endproc
+.LFE1:
 	.size	write, .-write
 	.align	2
 	.globl	_start
 	.type	_start, @function
 _start:
+.LFB2:
+	.loc 1 27 15
+	.cfi_startproc
 	addi	sp,sp,-32
+	.cfi_def_cfa_offset 32
 	sw	ra,28(sp)
 	sw	s0,24(sp)
+	.cfi_offset 1, -4
+	.cfi_offset 8, -8
 	addi	s0,sp,32
+	.cfi_def_cfa 8, 0
+	.loc 1 28 18
 	call	main
 	sw	a0,-20(s0)
+	.loc 1 29 3
 	lw	a0,-20(s0)
 	call	exit
+	.cfi_endproc
+.LFE2:
 	.size	_start, .-_start
+.Letext0:
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.4byte	0xbc
+	.2byte	0x4
+	.4byte	.Ldebug_abbrev0
+	.byte	0x4
+	.byte	0x1
+	.4byte	.LASF5
+	.byte	0xc
+	.4byte	.LASF6
+	.4byte	.LASF7
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	.Ldebug_line0
+	.byte	0x2
+	.4byte	.LASF8
+	.byte	0x1
+	.byte	0x1b
+	.byte	0x6
+	.4byte	.LFB2
+	.4byte	.LFE2-.LFB2
+	.byte	0x1
+	.byte	0x9c
+	.4byte	0x4b
+	.byte	0x3
+	.4byte	.LASF9
+	.byte	0x1
+	.byte	0x1c
+	.byte	0x7
+	.4byte	0x4b
+	.byte	0x2
+	.byte	0x91
+	.byte	0x6c
+	.byte	0
+	.byte	0x4
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.byte	0x5
+	.4byte	.LASF2
+	.byte	0x1
+	.byte	0xe
+	.byte	0x6
+	.4byte	.LFB1
+	.4byte	.LFE1-.LFB1
+	.byte	0x1
+	.byte	0x9c
+	.4byte	0x96
+	.byte	0x6
+	.4byte	.LASF0
+	.byte	0x1
+	.byte	0xe
+	.byte	0x10
+	.4byte	0x4b
+	.byte	0x2
+	.byte	0x91
+	.byte	0x6c
+	.byte	0x6
+	.4byte	.LASF1
+	.byte	0x1
+	.byte	0xe
+	.byte	0x22
+	.4byte	0x96
+	.byte	0x2
+	.byte	0x91
+	.byte	0x68
+	.byte	0x7
+	.string	"__n"
+	.byte	0x1
+	.byte	0xe
+	.byte	0x2d
+	.4byte	0x4b
+	.byte	0x2
+	.byte	0x91
+	.byte	0x64
+	.byte	0
+	.byte	0x8
+	.byte	0x4
+	.4byte	0x9c
+	.byte	0x9
+	.byte	0xa
+	.4byte	.LASF3
+	.byte	0x1
+	.byte	0x3
+	.byte	0x6
+	.4byte	.LFB0
+	.4byte	.LFE0-.LFB0
+	.byte	0x1
+	.byte	0x9c
+	.byte	0x6
+	.4byte	.LASF4
+	.byte	0x1
+	.byte	0x3
+	.byte	0xf
+	.4byte	0x4b
+	.byte	0x2
+	.byte	0x91
+	.byte	0x6c
+	.byte	0
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.byte	0x1
+	.byte	0x11
+	.byte	0x1
+	.byte	0x25
+	.byte	0xe
+	.byte	0x13
+	.byte	0xb
+	.byte	0x3
+	.byte	0xe
+	.byte	0x1b
+	.byte	0xe
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x10
+	.byte	0x17
+	.byte	0
+	.byte	0
+	.byte	0x2
+	.byte	0x2e
+	.byte	0x1
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x96,0x42
+	.byte	0x19
+	.byte	0x1
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x3
+	.byte	0x34
+	.byte	0
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x4
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0x8
+	.byte	0
+	.byte	0
+	.byte	0x5
+	.byte	0x2e
+	.byte	0x1
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x27
+	.byte	0x19
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x97,0x42
+	.byte	0x19
+	.byte	0x1
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x6
+	.byte	0x5
+	.byte	0
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x7
+	.byte	0x5
+	.byte	0
+	.byte	0x3
+	.byte	0x8
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x8
+	.byte	0xf
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x9
+	.byte	0x26
+	.byte	0
+	.byte	0
+	.byte	0
+	.byte	0xa
+	.byte	0x2e
+	.byte	0x1
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x27
+	.byte	0x19
+	.byte	0x87,0x1
+	.byte	0x19
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x97,0x42
+	.byte	0x19
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.4byte	0x1c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	0
+	.4byte	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF7:
+	.string	"/home/asm/Documents/An Introduction to Assm Prog with RISC-V/02 - Exercise 1.2"
+.LASF9:
+	.string	"ret_code"
+.LASF6:
+	.string	"file2.c"
+.LASF5:
+	.string	"GNU C17 10.2.0 -march=rv32i -mabi=ilp32 -g"
+.LASF3:
+	.string	"exit"
+.LASF1:
+	.string	"__buf"
+.LASF4:
+	.string	"code"
+.LASF2:
+	.string	"write"
+.LASF0:
+	.string	"__fd"
+.LASF8:
+	.string	"_start"
 	.ident	"GCC: () 10.2.0"
